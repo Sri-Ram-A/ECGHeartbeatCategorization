b6728f14c4bf351cf79875bbd7bb04d98bea5a20
Sri-Ram-A <srirnsametalmart@gmail.com>
2025-12-18 16:09:53 +0000

    NGROK😃

--- frontend/services/api.ts
+++ frontend/services/api.ts
@@ -1,2 +1,2 @@
 // export const BACKEND_URL = 'http://localhost:8000'
-export const BACKEND_URL = 'cub-true-shiner.ngrok-free.app'
+export const BACKEND_URL = 'https://cub-true-shiner.ngrok-free.app'
